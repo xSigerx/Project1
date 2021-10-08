06cef81fbdbb3391fc2d6937d764bce30c76f1fb
xSigerx <regis.snp@gmail.com>
2021-10-07 22:29:56 -0300

    aula 2 - rsn

---
+++ Projeto/at2_RSN.js
@@ -0,0 +1,9 @@
+//desenvolver um programa nojava que soma, dividir e multiplicar - colocar NO SEU REPÓSITÓRIO
+var valora = 2;
+var valorb = 4;
+var valorc = 6;
+var total;
+total = ((valora + valora) / valorb) * valorc;
+
+console.log(total);
+
